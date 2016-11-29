4bc69f21c9e23e7d3dac0ec952874fadd8bb21ea
dajaj <aurele.dousselin@centrale-marseille.fr>
2016-11-29 19:24:59 +0100

    fixup! fixup! Iter an action recursively through a directory

--- iter.py
+++ iter.py
@@ -1,19 +1,19 @@
 import mido_encodage as me
 import os
 
-def splitChannels(midiName,midiSource):
-	outDir = 'channels/' + midiSource
-	if not os.path.exists(outDir):
-	  os.makedirs(outDir)
-	
-	outDir = outDir + '/' + midiName[:-4]
-	if not os.path.exists(outDir):
-	  os.makedirs(outDir)
-	
-	midiFile = 'MIDI/' + midiSource + '/' + midiName
-	
-	channels,metas=me.parseMidi(midiFile)
-	me.saveMidiList(channels,outDir+'/chan_')
+#def splitChannels(midiName,midiSource):
+#	outDir = 'channels/' + midiSource
+#	if not os.path.exists(outDir):
+#	  os.makedirs(outDir)
+#	
+#	outDir = outDir + '/' + midiName[:-4]
+#	if not os.path.exists(outDir):
+#	  os.makedirs(outDir)
+#	
+#	midiFile = 'MIDI/' + midiSource + '/' + midiName
+#	
+#	channels,metas=me.parseMidi(midiFile)
+#	me.saveMidiList(channels,outDir+'/chan_')
 
 def printFile(file):
 	print(file)
@@ -25,28 +25,26 @@ def scanDir(path="./",directory=".",action="printFile"):
 		else:
 			globals()[action](path+directory+"/"+file)
 
-def feedRNNwithMIDI(midiFile):
+def addMidiToList(midiFile,midiList):
 	try:
 		channels,metas=me.parseMidi(midiFile)
 		for channel in channels:
-			feedRNNwithChannel(channel)
+			midiList.append(channel)
 	except:
 		os.remove(midiFile)
 		print(midiFile+" REMOVED")
 
-def feedRNNwithChannel(channel):
-	print("feeding")
-
 
 if __name__ == "__main__":
-	for sourceDir in os.listdir('MIDI'):
-		print(sourceDir)
-		for midiFile in os.listdir('MIDI/'+sourceDir):
-			if not os.path.exists('channels/'+sourceDir+'/'+midiFile[:-4]):
-				print('    '+midiFile)
-				try:
-					splitChannels(midiFile,sourceDir)
-				except:
-					os.remove('MIDI/'+sourceDir+'/'+midiFile)
-					os.rmdir('channels/'+sourceDir+'/'+midiFile[:-4])
-					print('      --> REMOVED')
+#	for sourceDir in os.listdir('MIDI'):
+#		print(sourceDir)
+#		for midiFile in os.listdir('MIDI/'+sourceDir):
+#			if not os.path.exists('channels/'+sourceDir+'/'+midiFile[:-4]):
+#				print('    '+midiFile)
+#				try:
+#					splitChannels(midiFile,sourceDir)
+#				except:
+#					os.remove('MIDI/'+sourceDir+'/'+midiFile)
+#					os.rmdir('channels/'+sourceDir+'/'+midiFile[:-4])
+#					print('      --> REMOVED')
+	print("Allow to iter an action through the files of a directory.")
