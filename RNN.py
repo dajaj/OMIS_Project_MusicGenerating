c2f1013559f844bcc2baeda3d7686c7ac42f8ca2
dajaj <aurele.dousselin@centrale-marseille.fr>
2016-11-30 13:17:43 +0100

    test

--- RNN.py
+++ RNN.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 
 print(":: Data set :")
-sample = preprocessMidi("MIDI/storth")
+sample = preprocessMidi("MIDI/test")
 
 X = np.array(sample)
 y = np.array(sample)
