7bc9d8be6d77ae90151d8c13b2bf605f80544a29
dajaj <aurele.dousselin@centrale-marseille.fr>
2017-01-18 14:10:27 +0100

    Update RNN.py

--- RNN.py
+++ RNN.py
@@ -5,13 +5,13 @@ from keras.models import Sequential
 from keras.layers.embeddings import Embedding
 import numpy as np
 
+saveName = "2017-01-18-model-001"
+
 print(":: IMPORTING DATA SET")
-X,y = preprocessMidi("MIDI/test",verbose=1,removeExceptions=False,max_sample_len=100,allowMultipleNotesOnTempo=False,allowNoteOnSeveralTempos=False)
+X,y = preprocessMidi("MIDI/test",verbose=1,removeExceptions=True,max_sample_len=20,allowMultipleNotesOnTempo=False,allowNoteOnSeveralTempos=False)
 
 if len(X) == 0:
   raise Exception("The sample is empty.")
-
-y = np.concatenate((y,y),axis=2) # Can't have a 1-dim output
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.20)
 
@@ -19,7 +19,7 @@ print("Data set splitted into train and test data")
 
 model = Sequential()
 model.add(Embedding(input_dim=128,output_dim=128))
-model.add(SimpleSeq2Seq(input_dim=128,output_dim=2,output_length=len(X[0])))
+model.add(SimpleSeq2Seq(input_dim=128,output_dim=128,output_length=len(X[0])))
 
 print(":: COMPILING MODEL")
 model.compile(loss='mse',optimizer='rmsprop')
@@ -31,3 +31,5 @@ print(":: EVALUATING MODEL ON TEST DATA")
 loss = model.evaluate(X_test,y_test)
 
 print("Loss : "+str(loss))
+
+model.save(saveName)
